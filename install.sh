a0338cb4149cbc6495c5983bf2f0ff88df23d549
Hugo SCHOCH <hugo.schoch@epitech.eu>
2016-06-29 14:20:56 +0200

    sudo echo 'It works'

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-rm -rf /usr/local/bin/eblih /usr/local/bin/eblih_srcs /tmp/eblih.tar.gz
+for _file in /usr/local/bin/eblih /usr/local/bin/eblih_srcs /tmp/eblih.tar.gz
+do
+	[ -w $_file ] && rm -rf $_file || sudo rm -rf $_file
+done
 
 curl -sL https://github.com/hug33k/eblih/archive/v2.0.0.tar.gz > /tmp/eblih.tar.gz
 
-mkdir /usr/local/bin/eblih_srcs
+if [ -w /usr/local/bin ]; then
+	mkdir /usr/local/bin/eblih_srcs
+	tar xf /tmp/eblih.tar.gz -C /usr/local/bin/eblih_srcs --strip=1
+	ln -s /usr/local/bin/eblih_srcs/eblih /usr/local/bin/eblih
+else
+	sudo mkdir /usr/local/bin/eblih_srcs
+	sudo tar xf /tmp/eblih.tar.gz -C /usr/local/bin/eblih_srcs --strip=1
+	sudo ln -s /usr/local/bin/eblih_srcs/eblih /usr/local/bin/eblih
+fi
 
-tar xf /tmp/eblih.tar.gz -C /usr/local/bin/eblih_srcs --strip=1
-
-ln -s /usr/local/bin/eblih_srcs/eblih /usr/local/bin/eblih
+rm -rf /tmp/eblih.tar.gz
